bba05735ea8deb72400748421153c9239b62fbe8
jpin730 <jaime.pineda.hqz@gmail.com>
2020-10-12 17:38:17 -0600

    Typo *footer in prices. Center 90% width prices and contact footer. Reduce size of icons for tablet and mobile. Changing x.svg. Opacity 1 for more info anchors in prices (hover) for tablet and mobile.

--- public/app/menu.js
+++ public/app/menu.js
@@ -8,10 +8,10 @@ function toggleMenuDisplay() {
     hideMenu = !hideMenu;
     if (hideMenu) {
       document.getElementById("menu-list").style.left = "100vw";
-      document.getElementById("menuBtn").style = "background: url('img/menu.svg'); background-size: 40px 40px;";
+      document.getElementById("menuBtn").style = "background: url('img/menu.svg') 50% 50% / 30px 30px no-repeat";
     } else {
       document.getElementById("menu-list").style.left = "0";
-      document.getElementById("menuBtn").style = "background: url('img/x.svg'); background-size: 40px 40px";
+      document.getElementById("menuBtn").style = "background: url('img/x.svg') 50% 50% / 25px 25px no-repeat";
     }
   }
 }
